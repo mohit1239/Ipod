8f108e842155ad216004e60a9de5174d268c7bc1
mohit1239 <msehrawat110@gmail.com>
2020-08-02 14:51:14 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Ipod from "./Ipod";
+
+function App() {
+  // calling the Ipod Component
+  return (
+    <div className="App">
+      <Ipod />
+    </div>
+  );
+}
+
+export default App;
